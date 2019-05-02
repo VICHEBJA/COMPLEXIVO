ef4691f794f57f7943d80e3ae0afb14e85bf972e
vichebja <vichebja@vichebjacp>
2019-05-02 16:41:28 -0500

    SISTEMA MENU EN CLASES 2.0

--- proyectocomplevichebj/src/ec/edu/vista/Aplicacion.java
+++ proyectocomplevichebj/src/ec/edu/vista/Aplicacion.java
@@ -1,11 +1,38 @@
 package ec.edu.vista;
 
+import java.util.Scanner;
+
 public class Aplicacion {
     public static void main(String[] args) {
      //Aplicacion.metodoA();
      //System.out.println("NUMERO RANDOMICO\n"+metodoA());
-        validarpares(); 
-        factorial();
+        Scanner entrada = new Scanner(System.in);
+     int v=0; int h=1;
+        while(v!=h)
+        {  
+            System.out.println("MENU\n"
+                    + "1-VALIDAR NUMEROS PAR/IMPAR\n"
+                    + "2-NUMERO FACTORIAL\n"
+                    + "3-NUMERO RANDOMICOS\n"                    
+                    + "4-SALIR");         
+       int op= entrada.nextInt();   
+       switch(op)
+               {
+             case 1: validarpares();
+             break;
+             case 2: factorial();
+             break;
+             case 4: v++;
+             break;
+             case 3: metodoA();
+             break;
+             default :  
+             break;/* break; */
+            }
+                    
+        
+        
+        } v--;     
     }
     public  static int metodoA()
     { //return 5;
