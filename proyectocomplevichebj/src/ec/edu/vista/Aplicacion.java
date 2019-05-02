cd40b33316d52399d7de4e7fb58c8ae443f9a5cb
VICHEBJA <49936614+VICHEBJA@users.noreply.github.com>
2019-05-02 17:11:44 -0500

    Add files via upload

--- proyectocomplevichebj/src/ec/edu/vista/Aplicacion.java
+++ proyectocomplevichebj/src/ec/edu/vista/Aplicacion.java
@@ -1,11 +1,41 @@
 package ec.edu.vista;
 
+import java.util.Scanner;
+
 public class Aplicacion {
     public static void main(String[] args) {
      //Aplicacion.metodoA();
      //System.out.println("NUMERO RANDOMICO\n"+metodoA());
-        validarpares(); 
-        factorial();
+        Scanner entrada = new Scanner(System.in);
+     int v=0; int h=1;
+        while(v!=h)
+        {  
+            System.out.println("MENU\n"
+                    + "1-VALIDAR NUMEROS PAR/IMPAR\n"
+                    + "2-NUMERO FACTORIAL\n"
+                    + "3-NUMERO RANDOMICOS\n"                    
+                    + "4-SALIR\n"
+                    + "5-EJEMPLO POTENCIA LIBRO");         
+       int op= entrada.nextInt();   
+       switch(op)
+               {
+             case 1: validarpares();
+             break;
+             case 2: factorial();
+             break;             
+             case 3: metodoA();
+             break;
+             case 4: v++;             
+             break;
+             case 5:potencia();
+             break;
+             default :  
+             break;/* break; */
+            }
+                    
+        
+        
+        } v--;     
     }
     public  static int metodoA()
     { //return 5;
@@ -24,7 +54,20 @@ public class Aplicacion {
     int acum= 1;
      for (int i=1;i<=x;i++)
          acum*=i;
-    System.out.println(acum+"\tES el factorial de "+x);
-     
+    System.out.println(acum+"\tES el factorial de "+x);     
     }
+    public static void potencia()
+    { int r,e; int n=0;int p;
+    Scanner entrada = new Scanner(System.in);
+    System.out.println("INGRESE NUMERO A POTENCIAR PAG 18");
+    n=entrada.nextInt();
+    System.out.println("INGRESE ELEVACION DE POTENCIA PAG 18");
+    p=entrada.nextInt();
+    e=p; r=1;
+    for(;p>0;p--)
+        r=r*n;
+    System.out.println("Potencia de:"+n+"¨"+e+"="+r);
+        
+    }
+    
 }
